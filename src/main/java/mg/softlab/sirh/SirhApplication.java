4a271350fed0b2127acc7fd187abc101763afe11
Mandresy Rabenja <mandresyrabenj@gmail.com>
2022-09-28 11:48:32 +0300

    Add Spring intlzr

---
+++ src/main/java/mg/softlab/sirh/SirhApplication.java
@@ -0,0 +1,13 @@
+package mg.softlab.sirh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/softlab/sirh/SirhApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.softlab.sirh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
